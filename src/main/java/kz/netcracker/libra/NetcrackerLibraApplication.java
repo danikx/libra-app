e7266a98191d01c82d9ba585a1ea8c4ff275e230
daniyar.kalmurzin <daniyar.kalmurzin@gmail.com>
2025-02-15 21:15:50 +0500

    init

---
+++ src/main/java/kz/netcracker/libra/NetcrackerLibraApplication.java
@@ -0,0 +1,12 @@
+package kz.netcracker.libra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetcrackerLibraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetcrackerLibraApplication.class, args);
+	}
+}
---
+++ src/test/java/kz/netcracker/libra/NetcrackerLibraApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.netcracker.libra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetcrackerLibraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
