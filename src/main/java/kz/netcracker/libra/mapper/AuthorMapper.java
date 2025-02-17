68bcd204be249439f5fbc16cbf17bc9d4eae176a
daniyar.kalmurzin <daniyar.kalmurzin@gmail.com>
2025-02-17 07:23:59 +0500

    bugfix

--- src/main/java/kz/netcracker/libra/mapper/AuthorMapper.java
+++ src/main/java/kz/netcracker/libra/mapper/AuthorMapper.java
@@ -3,6 +3,7 @@ package kz.netcracker.libra.mapper;
 import kz.netcracker.libra.dto.AuthorDto;
 import kz.netcracker.libra.entity.Author;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
@@ -10,7 +11,9 @@ public interface AuthorMapper {
 
     AuthorDto toDto(Author author);
 
+    @Mapping(target = "id", ignore = true) 
     Author toEntity(AuthorDto authorDto);
 
+    @Mapping(target = "id", ignore = true)
     void updateEntity(@MappingTarget Author author, AuthorDto authorDto);
 }
